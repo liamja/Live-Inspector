15d3855742ccc2e14773e5477fcb1201061af22f
Liam Anderson <liam.anderson.91@gmail.com>
2012-04-19 19:49:02 +0100

    Added file upload

--- main.rb
+++ main.rb
@@ -1,22 +1,29 @@
 class Main < Sinatra::Base
 
-  helpers Sinatra::Partials
+	helpers Sinatra::Partials
 
-  configure do
-    # Configure public directory
-    set :public_folder, File.join(File.dirname(__FILE__), 'public')
+	configure do
+		# Configure public directory
+		set :public_folder, File.join(File.dirname(__FILE__), 'public')
 
-    # Configure HAML and SASS
-    set :haml, { :format => :html5 }
-    set :sass, { :style => :compressed } if ENV['RACK_ENV'] == 'production'
-  end
+		# Configure HAML and SASS
+		set :haml, { :format => :html5 }
+		set :sass, { :style => :compressed } if ENV['RACK_ENV'] == 'production'
+	end
 
-  get '/' do
-    haml :index
-  end
+	get '/' do
+		haml :index
+	end
 
-  not_found do
-    halt 404, File.read(File.join(settings.public_folder, '404.html'))
-  end
+	post '/' do
+		File.open(settings.public_folder + '/uploads/' + params['file'][:filename], "w") do |f|
+			f.write(params['file'][:tempfile].read)
+		end
+		return "The file was successfully uploaded!"
+	end
+
+	not_found do
+		halt 404, File.read(File.join(settings.public_folder, '404.html'))
+	end
 
 end
