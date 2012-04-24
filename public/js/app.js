1a8e5a6e9ae0c42f0104eeb42a84ad23070ca247
Liam Anderson <liam.anderson.91@gmail.com>
2012-04-24 22:06:33 +0100

    Various UI additions

--- public/js/app.js
+++ public/js/app.js
@@ -1,8 +1,11 @@
 $(document).ready(function(){
 
-	$('input[name="file"]').click(function(e) {
-		e.preventDefault();
-		mixpanel.track('Project File Uploaded');
+	$('input[type="file"]').click(function(e) {
+		mixpanel.track('File Selected');
+	});
+
+	$('button[type="submit"]').click(function(e) {
+		mixpanel.track('File Uploaded');
 	});
 
 });
